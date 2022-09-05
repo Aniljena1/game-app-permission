cd63b92b59659f864ee216fdc5c9d40b9a740e4d
Anil Kumar Jena <55645579+Aniljena1@users.noreply.github.com>
2022-09-05 14:58:48 +0530

    third changes

--- utils/location.js
+++ utils/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyDLGxQV9fGzCZd-u0aNodDZDwbj9Z2Hclg'
+const GOOGLE_API_KEY = ''
 
 export function getMapPreview(lat,lng) {
     const imagePreview = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${lng}&key=${GOOGLE_API_KEY}`
